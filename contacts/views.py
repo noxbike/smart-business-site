5d0d0cd8e8e7f0ec545869ab478845e34a570b8c
noxbike <noxbike@gmail.com>
2026-01-06 20:59:00 +0400

    add automate email to the admin and the client when the client submit the form

--- contacts/views.py
+++ contacts/views.py
@@ -1,12 +1,35 @@
 from django.shortcuts import render
 from .forms import ContactForm
+from django.core.mail import send_mail
 
 # Create your views here.
 def contact(request):
     form = ContactForm()
+    print(form)
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
+            name=form.cleaned_data['name']
+            email=form.cleaned_data['email']
+            message=form.cleaned_data['message']
             form.save()
+
+            send_mail(
+                subject="Thank you for your message",
+                message="we have received your message and will get back to you shortly.",
+                from_email="noxbike@gmail.com",
+                recipient_list=[email],
+                fail_silently=True,
+            )
+
+            send_mail(
+                subject="New Contact Message Received",
+                message=f"Message from {name}:\n\n{message}",
+                from_email="noxbike@gmail.com",
+                recipient_list=["jamesbond00439@gmail.com"],
+                fail_silently=True,
+            )
             return render(request, 'contacts/contact_success.html')
-    return render(request, 'contacts/contact.html', {'form': form})
+    else:
+        form = ContactForm()    
+        return render(request, 'contacts/contact.html', {'form': form})
--- smart_site/settings.py
+++ smart_site/settings.py
@@ -11,11 +11,15 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+# Load environment variables
+env = environ.Env(DEBUG=(bool, False))
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
@@ -120,3 +124,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env('HOST_EMAIL')
+EMAIL_HOST_PASSWORD = env('HOST_PASSWORD')
