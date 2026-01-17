6d406186621e23248d7baba4d42d155d1350ecf7
noxbike <noxbike@gmail.com>
2026-01-17 21:45:50 +0400

    add dockerfile and docker-compose

---
+++ app/core/context.py
@@ -0,0 +1,10 @@
+def site_info(request):
+    return {
+        'SITE_NAME': 'Your Business',
+        "SITE_TAGLINE": "We help your business grow",
+        "SITE_DESCRIPTION": "Professional services to elevate your business presence online.",
+        "SITE_CONTACT": "Get in touch with us",
+        "CONTACT_EMAIL": "contact@yourbusiness.com",
+        "CONTACT_PHONE": "+1-800-123-4567",
+        "ADDRESS_LINE1": "123 Business St.",
+    }
